1e6e87c0d3dde79a984fe82fa12594dd2fd8f0d5
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-05-08 18:06:42 -0300

    fixed: header layout

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,4 +1,3 @@
-import { CloseButton } from "../CloseButton";
 
 import { useState } from "react";
 import bugImageUrl from '../../assets/bug.png';
@@ -36,16 +35,15 @@ export type FeedbackType = keyof typeof feedbackTypes
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>();
 
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
       {!feedbackType
         ? (<FeedbackTypeStep setFeedbackType={setFeedbackType} />)
-        : (<FeedbackContentStep />)
+        : (<FeedbackContentStep feedbackType={feedbackType} handleClose={handleRestartFeedback} />)
       }
         
         <footer className="text-xs text-neutral-400">
